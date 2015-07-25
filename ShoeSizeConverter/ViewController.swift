edf8b089928f007e0ee05bec33618fc5be162f66
Daniel Zhang <Daniel@Daniels-MBP.attlocal.net>
2015-07-25 03:52:28 -0500

    Finished project

--- ShoeSizeConverter/ViewController.swift
+++ ShoeSizeConverter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+        
+        
+        var integerFromTextField = mensShoeSizeTextField.text.toInt()!
+     
+       // var sizeFromTextField = mensShoeSizeTextField.text
+       // let numberFromTextField = sizeFromTextField.toInt()
+       // var integerFromTextField = numberFromTextField!
+        
+        let conversionConstant = 30
+        integerFromTextField += conversionConstant
+        mensConvertedShoeSizeLabel.hidden = false
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+        mensShoeSizeTextField.resignFirstResponder()
+        }
+        
+        
+    }
 
-}
+
 
